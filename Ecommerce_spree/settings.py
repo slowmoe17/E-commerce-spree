82c9060b13aab2908d921a781cc59700e27c27e7
Selim Mohamed <slowmoe329@gmail.com>
2022-07-20 17:31:58 +0200

    ,

--- Ecommerce_spree/settings.py
+++ Ecommerce_spree/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ku)!eu*ouu+r+x*c&xb^#z=58ozh-2bsvncy^ov0gu$^&yjgo8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
